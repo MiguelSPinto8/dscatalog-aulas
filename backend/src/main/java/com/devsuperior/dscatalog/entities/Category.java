61837c461cbef32addd51ff8a452c0e093a57acf
Miguel S Pinto <Miguel.S.Pinto@timestamp.pt>
2024-11-06 20:03:06 +0000

    database

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -1,10 +1,22 @@
 package com.devsuperior.dscatalog.entities;
 
 import java.util.Objects;
-import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.io.Serializable;
+@Entity
+@Table(name="tb_category")
 public class Category implements Serializable {
 	private static final long serialVersionUID = 1L;
+	
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private long id;
 	private String name;
 	
