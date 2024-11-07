12b966ea56d8a9e4019415c55dbc9382a00e97a4
Miguel S Pinto <Miguel.S.Pinto@timestamp.pt>
2024-11-07 15:48:27 +0000

    Pagination

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -7,6 +7,8 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.mapping.AccessOptions.SetOptions.Propagation;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -27,12 +29,12 @@ public class CategoryService {
 	private CategoryRepository repository;
 	
 	@Transactional(readOnly = true)
-	public List <CategoryDTO> findAll(){
-		List<Category>list=repository.findAll();
-		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+	public Page <CategoryDTO> findAllPaged(PageRequest pageRequest){
+		Page<Category>list=repository.findAll(pageRequest);
+		return list.map(x -> new CategoryDTO(x));
 		
 	}
-
+	@Transactional(readOnly = true)
 	public CategoryDTO findById(Long id) {
 		Optional<Category> obj = repository.findById(id);
 		Category entity =obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
